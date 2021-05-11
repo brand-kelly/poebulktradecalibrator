48c15aff15525080444f1a8083b2a9591dea3fed
brandnola <63052225+brandnola@users.noreply.github.com>
2021-05-10 22:10:16 -0500

    Change opacity of row exchange after copying trade whisper

--- PoeBulkTradeCalibrator.user.js
+++ PoeBulkTradeCalibrator.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.pathofexile.com/trade/exchange/*/*
 // @grant       none
-// @version     3.0
+// @version     5.0
 // @author      brandnola
 // @description Forces PoE's bulk exchange slider bar to be at most the number of items a seller has in stock
 // ==/UserScript==
@@ -14,39 +14,37 @@ const config = { attributes: false, childList: true, subtree: true };
 
 const callback = function (mutationsList, observer) {
     for (const mutation of mutationsList) {
-        if (!mutation.target.querySelector('.slider-right')) {
+        if (mutation.target.className !== 'row exchange') {
             return;
         }
-
-        const characterNameArray = mutation.target.querySelector('textarea').childNodes[0].data.match(/[^@ ]+/);
+        const tradeBox = mutation.target;
+        const characterNameArray = tradeBox.querySelector('textarea').childNodes[0].data.match(/[^@ ]+/);
         if (characterNameArray) {
             const characterName = characterNameArray[0];
-            let accountName = (mutation.target.querySelector('.right .profile-link').children[0].innerText =
-                characterName);
+            let accountName = (tradeBox.querySelector('.right .profile-link').children[0].innerText = characterName);
         }
 
-        const childPriceBlockLength = mutation.target.querySelector('.price-block')?.children.length;
-        const whatYouGet =
-            mutation.target.querySelector('.price-block')?.children[childPriceBlockLength - 1].textContent;
-        const sellerStock = mutation.target.querySelector('.stock')?.children[0].textContent;
+        const childPriceBlockLength = tradeBox.querySelector('.price-block')?.children.length;
+        const whatYouGet = tradeBox.querySelector('.price-block')?.children[childPriceBlockLength - 1].textContent;
+        const sellerStock = tradeBox.querySelector('.stock')?.children[0].textContent;
 
         const whatYouGetInteger = parseInt(whatYouGet);
         const sellerStockInteger = parseInt(sellerStock);
 
         const sliderMax = Math.floor(sellerStockInteger / whatYouGetInteger);
         if (sliderMax) {
-            mutation.target.querySelector('input[type="range"]')?.setAttribute('max', String(sliderMax));
+            tradeBox.querySelector('input[type="range"]')?.setAttribute('max', String(sliderMax));
         }
 
-        if (!mutation.target.querySelector('.slider-right button')) {
+        if (!tradeBox.querySelector('.slider-right button')) {
             const btn = document.createElement('button');
             btn.className = 'btn btn-default';
             btn.style.color = '#e9cf9f';
             btn.style.fontFamily = 'Verdana, Arial, Helvetica, sans-serif';
             btn.style.marginLeft = '.75rem';
-            const inputElement = mutation.target.querySelector('input[type="range"]');
+            const inputElement = tradeBox.querySelector('input[type="range"]');
             btn.innerHTML = 'Max';
-            mutation.target.querySelector('.slider-right').appendChild(btn);
+            tradeBox.querySelector('.slider-right').appendChild(btn);
             btn.onclick = () => {
                 inputElement.value = inputElement.max;
                 inputElement.dispatchEvent(new Event('input', { bubbles: true, cancelable: true }));
@@ -56,6 +54,12 @@ const callback = function (mutationsList, observer) {
                 btn.disabled = true;
             }
         }
+
+        // Changes opacity of trade box when textarea is clicked
+        const formEvent = tradeBox.querySelector('textarea');
+        formEvent.onclick = () => {
+            tradeBox.style.opacity = 0.5;
+        };
     }
 };
 
